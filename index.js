a8cbc07227a1777e6e909475745dd9fe6a9388ab
Chadwick Koo <chadwickkoo@outlook.com>
2025-03-01 15:07:22 +0300

    modified index.js file

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a,b) {
+ return    a + b
+}
+//add ("1484","1")
+
+function subtract(a,b){
+return    a - b
+}
+//subtract ("408","1")
+
+function multiply(a,b){
+    return    a * b
+}
+//multiply ("182192","1")
+
+function divide(a,b){
+    return a / b
+}
+
+function increment(n) {
+    return n +=1
+}
+
+function decrement(n) {
+    return n -=1
+}
+
+function makeInt(string) {
+    return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
